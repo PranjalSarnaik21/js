ef7078d09263360f996384382d7a1c1d5d568e13
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-02-11 09:48:19 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+ +
+3) // code readability should be high
+/*
+Primitative DataType
+number = 2 to power 53
+bigint 
+string = ""
+boolean = true/false
+null = standalone value (empty value) 
+undefined = value is not assign 
+symbol = uniqueness
+*/
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // Object
