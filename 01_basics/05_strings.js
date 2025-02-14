a65876c427d0c2e3b63b61729020b465310eaa95
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-02-14 10:21:38 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "Pranjal"
+const repoCount = 5
+console.log(name + repoCount + "Value"); // Pranjal5Value
+
+console.log(`Hello my name is ${name} and repo count is ${repoCount}`); 
+//Hello my name is Pranjal and repo count is 5
