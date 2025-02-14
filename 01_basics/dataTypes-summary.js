097c04944b28d214eabc6143085a44c24447ce11
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-02-14 10:06:15 +0000

    stack heap

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -39,3 +39,26 @@ console.log(typeof(unknown)); // undefined
 // js is a dynamically typed langauge. This means that variable types are determined at runtime,
 // and you do not need to explicitly declare the type of a variable before using it.
 // you can assign different types of values to a variable during its lifetime.
+
+// Stack (Primitive) -- variable declare kiya hai uska copy milta hai, 
+// Heap (Non-Primitive) -- reference of original value if any changes happen it will change in memory
+
+//Stack eg
+let name1 = "Pranjal"
+let anotherName = name1
+anotherName = "Priya"
+
+console.log(name1); // Pranjal
+console.log(anotherName); // Priya
+
+// Heap eg
+let userOne = {
+    email: "pranjal@cogni.com",
+    upi: "pranjal@pyl"
+}
+let userTwo = userOne
+// to accecss the element in object
+ userTwo.email = "Priya@gmail.com"
+
+ console.log(userOne.email); // Priya@gmail.com
+ console.log(userTwo.email); // Priya@gmail.com
