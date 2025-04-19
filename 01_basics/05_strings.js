eb355865825320287857d7e22a0b93e0dbe5d7b9
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-04-19 14:15:52 +0000

    stringss

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,5 +2,6 @@ const name = "Pranjal"
 const repoCount = 5
 console.log(name + repoCount + "Value"); // Pranjal5Value
 
-console.log(`Hello my name is ${name} and repo count is ${repoCount}`); 
+// String Interpolation (backticks = `)
+console.log(`Hello my name is ${name.ToUpper} and repo count is ${repoCount}`); 
 //Hello my name is Pranjal and repo count is 5 
