ca69663827f3ac3d2f26112928f8eecaf59ecbac
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-04-21 07:54:39 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,7 +1,11 @@
 const name = "Pranjal"
 const repoCount = 5
-console.log(name + repoCount + "Value"); // Pranjal5Value
+// console.log(name + repoCount + "Value"); // Pranjal5Value
 
 // String Interpolation (backticks = `)
-console.log(`Hello my name is ${name.ToUpper} and repo count is ${repoCount}`); 
+// console.log(`Hello my name is ${name.toLowerCase()} and repo count is ${repoCount}`); 
 //Hello my name is Pranjal and repo count is 5 
+let fName = "Pranjal"
+let lName = "Sarnaik"
+console.log(fName + " " + lName);
+
