4917bb4143fe1af47217e71f7b50cb9c044e5f01
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-04-23 10:21:57 +0000

    functions

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -11,12 +11,35 @@ let lName = "Sarnaik"
 
 // Functions
 let GameName = new String ("PranjalS")
-console.log(GameName[0]);
-console.log(GameName.__proto__); // {} = object
-console.log(GameName.length); //8
-console.log(GameName.charAt(2)); // a
-console.log(GameName.indexOf('r')); // 1
+// console.log(GameName[0]);
+// console.log(GameName.__proto__); // {} = object
+// console.log(GameName.length); //8
+// console.log(GameName.charAt(2)); // a
+// console.log(GameName.indexOf('r')); // 1
 
+let GameName1 = new String ("Pranjal_sar")
+const newString = GameName1.substring(0,4); 
+console.log(newString); // Pran last letter is not included; not follow -tive number
+
+anotherName = GameName1.slice(-11,4);
+console.log(anotherName); // Pran
+
+newString1 = new String ("   Pranjal   ")
+console.log(newString1.trim());  // Pranjal (trimStart() and trimEnd())
+
+const url = "https://pranjal.com/pranjal%20sarnaik" 
+// when there is space is url it will change in value 
+console.log(url.replace('%20', '-')); // https://pranjal.com/pranjal-sarnaik
+console.log(url.includes("pranjal")); // true
+ 
+const str = ("Pr-an-ja-l")
+console.log(str.split("-")); //[ 'Pr', 'an', 'ja', 'l' ]
+
+
+
+
+
+ 
 
 
 
