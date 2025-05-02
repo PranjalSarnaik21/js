89109a4c64f87f4fcb11348029994cbe6df46116
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-05-02 17:01:48 +0000

    obj

---
+++ 02_basics/03_object.js
@@ -0,0 +1,23 @@
+// objects literals (all key has string datatype )
+// Declare the symbol and used as a key in json payload
+const mySym = Symbol("key1")
+const jsUser = {
+    name : "Pranjal",
+    age : 24,
+    location : "Jaipur",
+    email : "Pranjal@cognizant.com",
+    isLoggedIn : false,
+    lastlogingDays : ["Mon", "fri"],
+    "full Name" : "Pranjal sarnaik",
+    [mySym] : "myKey1"  
+}
+console.log(jsUser.name); // Pranjal
+console.log(jsUser["name"]); // Pranjal
+console.log(jsUser["full Name"]); // Pranjal sarnaik
+// console.log(jsUser.full name); wrong syntax
+console.log(jsUser[mySym]); // myKey1
+console.log(typeof(jsUser[mySym]));
+
+
+
+
