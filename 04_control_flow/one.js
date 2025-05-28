80064bf2888ea164ba32a530e3a363260e20cbbc
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-05-28 06:28:21 +0000

    ctrlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+// if statement
+// syntax (comparision is done in condition like <, >, <=, >=, !=, ==, ===, !==)
+// if(condition should be true){}
+if(2=="2"){
+    //console.log("executed");  // executed for number check
+}
+if(2===2){
+    console.log("executed");  // executed for type checking
+}
+
+const temperature = 41
+if (temperature>=40){
+    console.log("temp is less than 50");   
+}
+else{
+    console.log("temp is greater than 50")
+}
+console.log("executed")
