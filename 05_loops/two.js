b43dc9d4b371887bbc3ff2b57e45b96c8df9b622
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-07-07 17:38:09 +0000

    while , dowhile

---
+++ 05_loops/two.js
@@ -0,0 +1,27 @@
+// while
+let i = 0
+while (i<=6) {
+    console.log(`The value is ${i}`);
+    i= i + 2
+}
+
+let myArray = ["a","b","c","d"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(myArray[arr]);   
+    arr++
+}
+
+// do while --> 1st code (consloge log statement) will execute after that condition will check 
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++ 
+} while (score < 5);
+
+let scored = 11
+do {
+    console.log(`Score is ${scored}`);
+    score++ 
+} while (scored < 5);     // Score is 11
+
