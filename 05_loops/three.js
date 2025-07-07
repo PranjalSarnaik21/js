a6bb5dce252e2f9e2040f46499cdea0f461dc607
Pranjal Sarnaik <158582133+PranjalSarnaik21@users.noreply.github.com>
2025-07-07 17:49:57 +0000

    for of

---
+++ 05_loops/three.js
@@ -0,0 +1,12 @@
+// for of 
+// for of we can use on array , {} , "" , any dataType
+arr = [1,2,3,4,5]
+for (const num of arr) {
+    //console.log(num);   // no need to write long syntax like (condition, ++)
+}
+greetings = "Hello World"
+for (const greet of greetings) {
+    console.log(greet);    
+}
+
+// Maps
